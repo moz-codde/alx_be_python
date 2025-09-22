8f7281cac41ca49fbd7cd104a47834e64a2a8e85
Farouk Adegboyega <moz.codde@gmail.com>
2025-09-22 08:32:11 +0100

    corrected typo

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
-monthly_inc = float(input("Enter your monthly income: "))
-monthly_exp = float(input("Enter you total monthly expenses: "))
-monthly_savings = monthly_inc - monthly_exp
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter you total monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
 
 proj_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 print(f"Your monthly savings are ${int(monthly_savings)}.")
