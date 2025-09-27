767515c884368295bb45148356760782731b1a27
Farouk Adegboyega <moz.codde@gmail.com>
2025-09-27 20:11:03 +0100

    fix: matching regex

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -3,5 +3,6 @@ priority = input("Priority (high/medium/low):")
 time_bound = input("Is it time-bound? (yes/no):")
 
 match priority:
-    case "high" if time_bound == "yes":
-        print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
+    case "high":
+        if time_bound == "yes":
+            print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
