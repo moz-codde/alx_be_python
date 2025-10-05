38aa3a9a59a506f78f4c33af031e9bc21ea4ad71
Farouk Adegboyega <moz.codde@gmail.com>
2025-10-05 23:59:28 +0100

    Initial commit: unittest

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,30 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+
+class TestSimpleCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        self.assertEqual(self.calc.add(3, 2), 5)
+        self.assertEqual(self.calc.add(1, -2), -1)
+        self.assertEqual(self.calc.add(-2, -2), -4)
+    
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(4, 2), 2)
+        self.assertEqual(self.calc.subtract(4, -2), 6)
+        self.assertEqual(self.calc.subtract(-1, 3), -4)
+        self.assertEqual(self.calc.subtract(-2, -1), -1)
+    
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(3, 2), 6)
+        self.assertEqual(self.calc.multiply(7, 3), 21)
+        self.assertEqual(self.calc.multiply(8, 0), 0)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(8, 2), 4)
+        self.assertIsNone(self.calc.divide(5, 0))
+        self.assertIsInstance(self.calc.divide(7, 2), float)
+
+        
