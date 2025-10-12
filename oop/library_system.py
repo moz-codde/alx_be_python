c9121a049e240277884268d0d3f90e0f7637a162
Farouk Adegboyega <moz.codde@gmail.com>
2025-10-12 23:57:45 +0100

    Change the  methods to

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,7 +3,7 @@ class Book:
         self.title = title
         self.author = author
 
-    def __repr__(self):
+    def __str__(self):
         return f"Book: {self.title} by {self.author}"
     
 
@@ -12,7 +12,7 @@ class EBook(Book):
         super().__init__(title, author)
         self.file_size = file_size
     
-    def __repr__(self):
+    def __str__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}"
     
 
@@ -21,7 +21,7 @@ class PrintBook(Book):
         super().__init__(title, author)
         self.page_count = page_count
 
-    def __repr__(self):
+    def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
